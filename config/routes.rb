00d7c48af778c10b3934372ca7af21932890f14f
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2022-09-30 16:57:51 -0300

    Aula 131

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
+
   namespace :admins_backoffice do
     get 'welcome/index'
   end
+
   devise_for :profiles
   devise_for :admins
-  root 'welcome#index'
+
+  get 'inicio', to: 'site/welcome#index'
+
+  root 'site/welcome#index'
 end
