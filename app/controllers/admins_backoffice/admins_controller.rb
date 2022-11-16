7bd1c6a852059dc984e8d95b62034a76db1b60ce
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2022-11-16 17:04:30 -0300

    Aula 151

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,4 +6,16 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   def edit
     @admin = Admin.find(params[:id])
   end
+
+  def update
+    @admin = Admin.find(params[:id])
+      if @admin.update_attributes(params[:admin])
+        flash[:success] = "Object was successfully updated"
+        redirect_to @admin
+      else
+        flash[:error] = "Something went wrong"
+        render 'edit'
+      end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   namespace :admins_backoffice do
     resources :welcome, only: [:index]
-    resources :admins, only: [:index, :edit]
+    resources :admins, only: [:index, :edit, :update]
   end
 
   get 'inicio', to: 'site/welcome#index'
