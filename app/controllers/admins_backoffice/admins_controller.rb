12517aad53e6545d78a42e15f37e5bcd58b199d3
Thamires Falbo <111710331+thamiresfalbo@users.noreply.github.com>
2022-12-02 15:51:27 -0300

    Aula 155.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,20 +1,15 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: [:update]
+  before_action :set_admin, only: %i[edit update]
+
   def index
     @admins = Admin.all
   end
 
-  def edit
-    @admin = Admin.find(params[:id])
-  end
+  def edit; end
 
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password,:password_confirmation)
-    end
-
-    
-    @admin = Admin.find(params[:id])
-    if @admin.update(admin_parms)
+    if @admin.update(admin_params)
       redirect_to admins_backoffice_admins_index_path, notice: 'Admin was successfully updated'
     else
       render :edit, notice: 'Something went wrong'
@@ -23,7 +18,17 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
 
   private
 
-  def admin_parms
+  def verify_password
+    return unless params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+
+    params[:admin].extract!(:password, :password_confirmation)
+  end
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def admin_params
     params.require(:admin).permit(:email, :password, :password_confirmation)
   end
 end
